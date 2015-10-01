88fc3c929369ac2002ce671f02b2a02071502e9f
Paul Nitto <pnitto18@gmail.com>
2015-10-01 08:45:27 -0400

    Update 2015-10-01T12:45:24.194Z

--- app/scripts/models/contact-collection.js
+++
@@ -1,8 +0,0 @@
-var Contact = require('models/contact');
-
-var ContactsCollection = Backbone.Collection.extend({
-  model: Contact,
-  url: "http://tiny-lasagna-server.herokuapp.com/collections/pnlist"
-});
-
-module.exports = ContactsCollection;
